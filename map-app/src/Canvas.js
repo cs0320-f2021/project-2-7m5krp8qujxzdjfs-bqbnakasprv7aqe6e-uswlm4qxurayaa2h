bde088139293dfe8ff79fdd9a0d60cd43b1127c2
uswlm4qxurayaa2h <uswlm4qxurayaa2h@gmail.com>
2021-11-08 02:38:55 +0000

    blank canvas

--- map-app/src/Canvas.js
+++ map-app/src/Canvas.js
@@ -10,6 +10,13 @@ function Canvas(props) {
     useEffect(() => {
         if (canvasRef) {
             const ctx = canvasRef.current.getContext('2d')
+            ctx.fillStyle = '#3e3ed3';
+            ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+            ctx.moveTo(0, 0);
+            ctx.lineTo(500, 500);
+            ctx.moveTo(0, 0);
+
+            ctx.stroke();
             if (props.ways.current.ways) {
                 draw(ctx, props.ways.current.ways)
             }
--- map-app/src/Maps.js
+++ map-app/src/Maps.js
@@ -63,6 +63,7 @@ function Maps() {
                         })
                     }
                 })
+                .catch((err) => console.log(err))
         })
     }
 
