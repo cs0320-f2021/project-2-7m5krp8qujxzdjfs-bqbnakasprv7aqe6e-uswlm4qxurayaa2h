58b8cfac44351d540da30397581634b4d9e8f7e1
uswlm4qxurayaa2h <uswlm4qxurayaa2h@gmail.com>
2021-11-12 20:57:28 +0000

    fix: coords now are in right range

--- map-app/src/Canvas.js
+++ map-app/src/Canvas.js
@@ -4,13 +4,14 @@ function Canvas(props) {
     const canvasRef = useRef(null)
 
     const coordToXY = (coord, width, height) => {
-        const lon = coord[0]
-        const lat = coord[1]
-        const heightOffset = Math.abs(lat - props.minLat) / Math.abs(props.maxLat - props.minLat)
-        const widthOffset = Math.abs(lon - props.minLon) / Math.abs(props.maxLon - props.minLon)
-        const x = width * widthOffset
-        const y = height * heightOffset
-        console.log(x, y)
+        const lat = coord[0]
+        const lon = coord[1]
+        // const heightOffset = Math.abs(props.maxLat - lat) / Math.abs(props.maxLat - props.minLat)
+        // const widthOffset = Math.abs(lon - props.minLon) / Math.abs(props.maxLon - props.minLon)
+        const widthPercent = (lon - props.minLon) / (props.maxLon - props.minLon)
+        const heightPercent = (lat - props.minLat) / (props.maxLat - props.minLat)
+        const x = width * widthPercent
+        const y = height * heightPercent
         return [x, y]
     }
 
@@ -18,12 +19,16 @@ function Canvas(props) {
     //   ["name": "Thayer St", "startCoord": [lon, lat], "endCoord": [lon, lat]] }
     const draw = (ctx, canvasWays) => {
         ctx.beginPath()
+        ctx.strokeStyle = "#000000"
+        ctx.fillStyle = "#FFFFFF"
         console.log("canvasWays:", canvasWays)
         canvasWays.forEach((way) => {
+            console.log("holy shoot y'all")
             const startCoord = [way[1], way[2]]
             const endCoord = [way[3], way[4]]
             let startXY = coordToXY(startCoord, canvasRef.current.width, canvasRef.current.height)
             let endXY = coordToXY(endCoord, canvasRef.current.width, canvasRef.current.height)
+            console.log("coordxys", startXY, endXY)
             ctx.moveTo(startXY[0], startXY[1])
             ctx.lineTo(endXY[0], endXY[1])
         })
